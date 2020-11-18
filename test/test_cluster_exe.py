1ca7da28bb5f33bab7398215bcb51d6a87967484
zouzhicun <2515717245@qq.com>
2020-11-18 16:49:02 +0900

    update

--- test/test_cluster_exe.py
+++ test/test_cluster_exe.py
@@ -11,15 +11,6 @@ from core.cluster_exe import ClusterExecution
 
 
 if __name__=="__main__":
-    #redis_tmp_pool=redis.ConnectionPool(host="127.0.0.1", port=6379, db=2, password="my_passwd",decode_responses=True)
-    #redis_send_pool=redis.ConnectionPool(host="127.0.0.1", port=6379, db=0, password="my_passwd",decode_responses=True)
-    #redis_log_pool=redis.ConnectionPool(host="127.0.0.1", port=6379, db=1, password="my_passwd",decode_responses=True)
-    #redis_config_pool=redis.ConnectionPool(host="127.0.0.1", port=6379, db=15, password="my_passwd",decode_responses=True)
-    #
-    #redis_send_client=redis.StrictRedis(connection_pool=redis_send_pool)
-    #redis_log_client=redis.StrictRedis(connection_pool=redis_log_pool)
-    #redis_tmp_client=redis.StrictRedis(connection_pool=redis_tmp_pool)
-    #redis_config_client=redis.StrictRedis(connection_pool=redis_config_pool)
     
     #可清除以下
     redis_send_config={"host":"127.0.0.1", "port":6379, "db":0, "password":"my_redis_passwd"}
