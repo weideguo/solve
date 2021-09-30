1b59bfcbb6902ebda4f90c021bd3be9d249f2918
weideguo <2515717245@qq.com>
2021-09-30 16:34:04 +0800

    chore: docker-entrypoint.sh update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,9 +5,10 @@
 # export REDIS_PASSWORD=my_redis_passwd
 #
 cd /data/solve
-sed -i "s|127.0.0.1|${REDIS_HOST}|g" ./conf/config.conf
-sed -i "s|6379|${REDIS_PORT}|g" ./conf/config.conf
 sed -i "s|my_redis_passwd|${REDIS_PASSWORD}|g" ./conf/config.conf
+if [ "X${REDIS_HOST}X" != "XX" ]; then      sed -i "s|127.0.0.1|${REDIS_HOST}|g" ./conf/config.conf;fi
+if [ "X${REDIS_PORT}X" != "XX" ]; then      sed -i "s|6379|${REDIS_PORT}|g" ./conf/config.conf;fi
+if [ "X${AES_KEY}X" != "XX" ]; then         sed -i "s|aes_key=.*|aes_key=${AES_KEY}|g" ./conf/config.conf;fi
 python bin/solve.py start 
 if [ ! -d logs ]; then mkdir logs; fi
 if [ ! -f logs/solve.out ]; then touch logs/solve.out; fi
